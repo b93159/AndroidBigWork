0c739b2547b619dcc71542e9f3bee18de94d966a
b93159 <s111mple6@sina.com>
2020-06-11 18:52:44 +0800

    AndroidBigWork

---
+++ AndroidBigWork/app/src/main/java/com/my/androidbigwork/MyAdapter.java
@@ -0,0 +1,97 @@
+package com.my.androidbigwork;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.MediaController;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.List;
+
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
+    private List<VideoInfo> videoData;
+    private Context mContext;
+    private static final int UPDATE_UI = 1;
+    private int oldProgress = 0;
+
+    public MyAdapter(Context context){
+        mContext = context;
+    }
+
+
+    // 每一个videoholder对应一个视频
+    public static class MyViewHolder extends RecyclerView.ViewHolder {
+        public MyVideo videoView;
+        public ImageButton btn_play;
+        public Button btn_pause;
+        public ImageView vedioImage;
+
+
+
+        public MyViewHolder(View v){
+            super(v);
+            videoView=v.findViewById(R.id.videoView);
+            btn_pause=v.findViewById(R.id.btn_pause);
+            btn_play=v.findViewById(R.id.btn_play);
+            vedioImage=v.findViewById(R.id.videoImage);
+        }
+    }
+
+    public void setData(List<VideoInfo> myDataset) { videoData = myDataset; }
+
+    @Override
+    public MyAdapter.MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item, parent, false);
+        final MyViewHolder myViewHolder = new MyViewHolder(v);
+        return myViewHolder;
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, int position) {
+        //get element from my dataset at this position
+        //replace the contents of the view with that element
+
+        holder.videoView.setVideoPath(videoData.get(position).feedurl);
+        holder.videoView.setMediaController(new MediaController(mContext));
+
+
+        Glide.with(mContext).load(videoData.get(position).avatar).placeholder(R.drawable.loading).into(holder.vedioImage);
+
+        //点击播放暂停
+        holder.videoView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                if (holder.videoView.isPlaying()){
+                    holder.videoView.pause();
+                }else {
+                    holder.videoView.start();
+                    holder.vedioImage.setVisibility(View.GONE);
+                }
+                return false;
+            }
+        });
+
+    }
+
+
+
+    // 返回item数
+    @Override
+    public int getItemCount() {
+        return videoData == null ? 0 : videoData.size();
+    }
+
+}
+
---
+++ AndroidBigWork/app/src/main/java/com/my/androidbigwork/VideoInfo.java
@@ -0,0 +1,20 @@
+package com.my.androidbigwork;
+
+import com.google.gson.annotations.SerializedName;
+
+public class VideoInfo {
+    @SerializedName("_id")
+    public String _id;
+    @SerializedName("feedurl")
+    public String feedurl;
+    @SerializedName("nickname")
+    public String nickname;
+    @SerializedName("description")
+    public String description;
+    @SerializedName("likecount")
+    public int likecount;
+    @SerializedName("avatar")
+    public String avatar;
+}
+
+
